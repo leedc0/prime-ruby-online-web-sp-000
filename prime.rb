23108181a65825944b218f33cb8633cd3a77876e
Daniel Lee <leedc@me.com>
2019-08-18 01:18:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,14 @@
 # Add  code here!
+# def prime?(num)
+#   (1..100).each do |n|
+#     return false if num % n == 0
+#   end
+#   true # + "Be sure to account for negative numbers!"
+# end
+
 def prime?(num)
-  (-1..100).each do |n|
+  (2..(num-1)).each do |n|
     return false if num % n == 0
   end
-  true # + "Be sure to account for negative numbers!"
+  true
 end
