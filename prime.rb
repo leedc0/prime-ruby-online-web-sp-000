a5c84752e57cb411d63bef1bac9b0ae8ed955337
Daniel Lee <leedc@me.com>
2019-08-18 02:05:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,11 @@
 # Add  code here!
-# def prime?(num)
-#   (1..100).each do |n|
-#     return false if num % n == 0
-#   end
-#   true # + "Be sure to account for negative numbers!"
-# end
 
 def prime?(num)
-  (2..(num-1)).each do |n|
-    return false if num % n == 0
+  if num < 2
+    return false
+  else
+    (2..(num-1)).each do |n|
+      return false if num % n == 0
   end
   true
 end
